928f0597ecb0f977a39f53dad4694813c0a8846e
Seb-sti1 <65665540+seb-sti1@users.noreply.github.com>
2024-04-07 21:26:48 +0200

    feat(icp): better but yet not working icp
    - Create class implementing ICP
    - Create class inhering Node to interface with ROS
    - Add tests on simple cases

--- colcon_workspace/robot_autonomy_seb/robot_autonomy_seb/icp.py
+++ colcon_workspace/robot_autonomy_seb/robot_autonomy_seb/icp.py
@@ -1,94 +1,176 @@
+import time
+
 import numpy as np
 import rclpy
+from geometry_msgs.msg import TwistStamped
 from rclpy.node import Node
 from sensor_msgs.msg import LaserScan
-from geometry_msgs.msg import Twist
+
+from .utils import rotation_to_angle, scan_to_points, transform
 
 
-def rotation(theta, points):
-    return np.matmul(np.array([[np.cos(theta), -np.sin(theta)], [np.sin(theta), np.cos(theta)]]), points)
+class ICP:
+    """
+    This class contains the function to execute the ICP algorithm. It is seperated from the
+    ICPNode class to make it easier to test (see test/test_icp.py).
+    """
+
+    def __init__(self, logger=None, clock=None):
+        """
+        Create the icp object. It creates some var that will be used later.
+
+        :param logger:
+        :param clock:
+        """
+
+        self.__logger__ = logger
+        self.__clock__ = clock
+        self.sources = None
+
+    def clock(self):
+        """
+        :return: Get the current time in milliseconds (either using ROS or time package)
+        """
+        if self.__clock__ is not None:
+            return self.__clock__.now().nanoseconds
+        return time.time()
+
+    def log(self, string):
+        """
+        :param string: the text to be logged
+        :return: Log the given string either to the ROS logger or using print
+        """
+
+        if self.__logger__ is not None:
+            self.__logger__.info(string) / 1000_000_000
+        else:
+            print(string)
+
+    def execute_icp(self, targets):
+        """
+        Compute the transformation between the previous supplied targets and the new one
+
+        :param targets: the new point cloud 2xN where N is the number of points in the point cloud
+        :return: None the first time, then a transformation matrix 3x3
+        """
+        T = None
+
+        if self.sources is not None:
+            T = np.eye(3)
+            b = 0
+            for i in range(20):
+                # find the closet point to each target point from the source (with the transformation of the previous step)
+                targets_matched, sources_matched = self.reorder(transform(T, self.sources), targets)
+                if np.mean(np.linalg.norm(sources_matched - targets_matched, axis=0)) < 0.001:
+                    break
+                # compute the transformation
+                T = np.dot(T, self.compute_transformation(sources_matched, targets_matched))
+
+            # self.log(f"mean dist {abs(b - np.mean(np.linalg.norm(sources_matched - targets_matched, axis=0)))}")
+        self.sources = targets
+        return T
+
+    def reorder(self, sources, targets, max_dist=0.5):
+        """
+        Computes an array sources_matched where sources_matched[:, i] is the closest point to targets[:, i] from self.sources
+
+        :param sources: the old point cloud
+        :param targets: the new point cloud
+        :param max_dist: the distance at which a correspondence is rejected (in meters)
+        :return: two point cloud targets_matched and sources_matched, where targets_matched[:, i] is the closest point
+                    to sources_matched[:, i]
+        """
+        targets_matched = np.ones(targets.shape, dtype=np.float32) * np.inf
+        sources_matched = np.ones(targets.shape, dtype=np.float32) * np.inf
+        idx = 0
+
+        for i in range(targets.shape[1]):
+            target = targets[:, i:i + 1]
+
+            distances = np.linalg.norm(sources - target, axis=0)
+            m = np.argmin(distances)
+            if distances[m] < max_dist:
+                targets_matched[:, i] = target[:, 0]
+                sources_matched[:, i] = sources[:, m]
+                idx += 1
+
+        return targets_matched[:, targets_matched[0, :] < np.inf], sources_matched[:, sources_matched[0, :] < np.inf]
+
+    def compute_transformation(self, source_matched, target_matched):
+        """
+        Use the svd algorithm to compute the transformation matrix
+
+        :param source_matched: the source point cloud 2xN where N is the number of points in the point cloud
+        :param target_matched: the target point cloud 2xN where N is the number of points in the point cloud
+        :return: the transformation matrix to transform the source point cloud into target point cloud
+        """
+
+        centroid_source = source_matched.mean(axis=1)[:, np.newaxis]
+        centered_source = source_matched - centroid_source
+        centroid_target = target_matched.mean(axis=1)[:, np.newaxis]
+        centered_target = target_matched - centroid_target
+
+        H = np.dot(centered_target, centered_source.T) / centered_source.shape[1]
+        U, S, Vh = np.linalg.svd(H)
+        R = np.dot(U, Vh)
+        t = centroid_target - np.dot(R, centroid_source)
+
+        T = np.zeros((3, 3), dtype=np.float32)
+        T[:2, :2] = R
+        T[:2, 2] = t[:, 0]
+        T[2, 2] = 1
+        return T
 
 
-def gradient(theta, last_points, current_points):
-    # [(Xi, Yi) ...]
-    rotated = rotation(theta, current_points)
-    # [(dXi/dtheta, dYi/dtheta) ... ]
-    derivative = np.matmul(np.array([[-np.sin(theta), -np.cos(theta)], [np.cos(theta), -np.sin(theta)]]),
-                           current_points)
-
-    return 2 * np.sum(np.multiply(rotated - last_points, derivative))
-
-
-class ICP(Node):
+class ICPNode(Node):
+    """
+    This is a small class to interface ICP to ROS
+    """
 
     def __init__(self):
         super().__init__('homemade_icp')
-        self.subscription = self.create_subscription(
-            LaserScan,
-            'scan',
-            self.listener_callback,
-            10)
+        self.subscription = self.create_subscription(LaserScan, 'scan', self.listener_callback, 10)
+        # TODO instead of a twist make a Odometry https://docs.ros.org/en/latest/api/nav_msgs/html/msg/Odometry.html
+        self.publisher = self.create_publisher(TwistStamped, 'lidar_twist', 10)
 
-        self.publisher = self.create_publisher(Twist, 'lidar_twist', 10)
-
-        self.last_points = None
+        self.icp = ICP()
         self.last_time = None
 
     def listener_callback(self, scan):
+        """
+        Callback when new lidar measurement is received
+        :param scan: the data (see https://docs.ros.org/en/latest/api/sensor_msgs/html/msg/LaserScan.html)
+        """
 
-        angles = np.array([scan.angle_min + i * scan.angle_increment for i, d in enumerate(scan.ranges)
-                           if scan.range_min <= d <= scan.range_max])[:10]
-        ranges = np.array([d for d in scan.ranges
-                           if scan.range_min <= d <= scan.range_max])[:10]
-        current_points = np.append([np.multiply(ranges, np.cos(angles))], [np.multiply(ranges, np.sin(angles))], axis=0)
+        t = scan.header.stamp.nanosec / 1000_000_000
+        targets = scan_to_points(scan)
 
-        if self.last_points is not None:
-            center_translation = np.mean(self.last_points, axis=1) - np.mean(current_points, axis=1)
-            shifted_points = current_points + center_translation[:, np.newaxis]
+        T = self.icp.execute_icp(targets)
+        if T is not None:
+            # TODO finish below
+            dt = t - self.last_time
+            self.get_logger().info(
+                f"{T[0, 2] / dt:.03f}, {T[1, 2] / dt:.03f}, {rotation_to_angle(T[:2, :2] * 180 / 3.14 / dt):.02f}")
 
-            # last_points = [ (bar_xi, bar_yi) ... ]
-            # shifted_points = [ (xi, yi) ...]
-            # rotation(theta, shifted_points) =  [ (Xi, Yi) ...]
-            #
-            # cost = sum_i [ (Xi - bar_xi)**2 + (Yi - bar_Yi)**2 ]
-            #
-            # dc/dtheta = sum_i [ 2 dXi/dtheta * (Xi - bar_xi) + 2 dYi/dtheta * (Yi - bar_yi) ]
-            # dXi/dtheta = -xi sin theta - yi cos theta
-            # dYi/dtheta = xi cos theta - yi sin theta
-            theta = 0
-            rate = 0.01
-            grad = gradient(theta, self.last_points, shifted_points)
-            i = 0
-
-            # Need tweaking
-            while abs(grad) > 0.1 and i < 1000:
-                theta = theta + rate * grad
-                i += 1
-
-            dt = (self.get_clock().now() - self.last_time).nanoseconds * 1e-9
-
-            twist = Twist()
-            twist.linear.x = center_translation[0]/dt
-            twist.linear.y = center_translation[1]/dt
-            twist.linear.z = 0.
-            twist.angular.z = theta/dt
+            twist = TwistStamped()
+            twist.header.stamp = scan.header.stamp
+            twist.header.frame_id = 'odom'
+            twist.twist.linear.x = T[0, 2] / dt
+            twist.twist.linear.y = T[1, 2] / dt
+            twist.twist.linear.z = 0.
+            twist.twist.angular.z = rotation_to_angle(T[:2, :2]) / dt
 
             self.publisher.publish(twist)
 
-        self.last_points = current_points
-        self.last_time = self.get_clock().now()
+        self.last_time = t
 
 
 def main(args=None):
     rclpy.init(args=args)
 
-    minimal_subscriber = ICP()
-
+    minimal_subscriber = ICPNode()
     rclpy.spin(minimal_subscriber)
 
-    # Destroy the node explicitly
-    # (optional - otherwise it will be done automatically
-    # when the garbage collector destroys the node object)
     minimal_subscriber.destroy_node()
     rclpy.shutdown()
 
