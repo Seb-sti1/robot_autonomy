9c9581bf64493e7bddf6fe75dc55be058934e248
Seb-sti1 <65665540+Seb-sti1@users.noreply.github.com>
2024-04-25 18:00:10 +0200

    feat(icp launch): refactor launch file

--- colcon_workspace/robot_autonomy_seb/launch/icp_standalone.launch.py
+++ colcon_workspace/robot_autonomy_seb/launch/icp_standalone.launch.py
@@ -8,25 +8,14 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 
 def generate_launch_description():
-    my_package = get_package_share_directory('robot_autonomy_seb')
-
-    my_turtlebot = get_package_share_directory('my_turtlebot')
-    my_turtlebot_launch_dir = os.path.join(my_turtlebot, 'launch')
-
-    nav2_params = os.path.join(my_package, 'params', 'nav2_params.yml')
-
-    ld = LaunchDescription()
-
-    # start turtlebot simulation
-    ld.add_action(IncludeLaunchDescription(PythonLaunchDescriptionSource(
-        os.path.join(my_turtlebot_launch_dir, 'turtlebot_simulation.launch.py')),
-        launch_arguments={'params_file': nav2_params,
-                          'rviz_config_file': os.path.join(my_package,
-                                                           'rviz',
-                                                           'icp.rviz')}.items()
-    ))
-
-    # start odom based on icp
-    ld.add_action(Node(package="robot_autonomy_seb",
-                       executable="icp"))
+    ld = LaunchDescription([
+        # start simulation
+        IncludeLaunchDescription(PythonLaunchDescriptionSource(
+            os.path.join(get_package_share_directory('robot_autonomy_seb'), 'launch',
+                         'sim_with_custom_configs.launch.py'))
+        ),
+        # start odom based on map
+        Node(package="robot_autonomy_seb",
+             executable="icp")
+    ])
     return ld
